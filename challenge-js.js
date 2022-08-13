9776a0f9ca643815fa90c3ad2ecc076c0c55f256
Wytoria Faustino <86625354+wytoriaa@users.noreply.github.com>
2022-08-13 20:47:08 -0300

    js code

--- challenge-js.js
+++ challenge-js.js
@@ -1,3 +1,5 @@
+const { title } = require("process")
+
 //Crie uma classe Book 
 class Book{
     constructor(title, author, description){
@@ -17,18 +19,18 @@ class Library{
         this.books = []
     }
 
-    //adicionar
+    //adicionar um livro
     add_book(bookInfo){
         const book_new = new Book(bookInfo.title, bookInfo.author, bookInfo.description)
         this.books.push(book_new)
     }
 
-    //mostrar um
+    //listar todos os livros
     get_book(){
         return this.books
     }
 
-    //remover
+    //remover um livro
     remove_book_id(bookID){
         let removeBook 
         this.books = this.books.filter(book =>{
@@ -40,4 +42,41 @@ class Library{
         })
         return removeBook
     }
+
+    //mostrar um livro
+    get_book_id(bookID){
+        let yourBook
+        this.books = this.books.filter(book =>{
+            if (book.id === bookID){
+                yourBook = book
+            } else{
+                return book
+            }
+        })
+    }
+
+    //editar um livro
+    update_book_id(id, info = {}){
+        let updateBook = this.get_book_id(id)
+        this.books = this.books.filter(book =>{
+            if(book.id === id){
+                book.title = info.title
+                book.description = info.description
+                book.author = info.author
+                return book
+            }
+        })
+        return updateBook
+    }
 }
+
+let library = new Library
+var book1 = new Book("O ceifador", "Neal", "Distopia")
+library.add_book(book1)
+
+var book2 = new Book ("Torto Arado", "Itamar Vieira", "Brasil atemporal")
+library.add_book(book2)
+library.get_book()
+library.remove_book_id(1)
+library.update_book_id(1, {title: "Os Testamentos", description: "Distopia", author: "Margaret"})
+console.log(book2)
